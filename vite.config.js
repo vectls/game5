a8027c8c0a740f81959bd72e8708fd074d461d44
shiro <st41ee@gmail.com>
2025-12-16 20:51:29 +0900

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // GitHub Pagesに対応するため、アセットへのパスを「相対パス」に設定します。
+  // 1. パス解決: GitHub Pagesのサブディレクトリに対応するため、相対パス './' を設定
   base: './', 
+  
+  // 2. 出力フォルダ: GitHub Pagesで許可されている 'docs' フォルダに出力先を変更
+  build: {
+    outDir: 'docs',
+  },
 });
